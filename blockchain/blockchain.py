d4c0db8fbf64e069a8840c8cc50756a12ca6c800
TonnyTran <anhtt17895@gmail.com>
2018-07-18 14:19:55 +0800

    v0.2

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -61,6 +61,7 @@ class BlockchainEnv(gym.Env):
 
     def reset(self):
         self.state = self.observation_space.sample()
+        print(self.state)
         self.steps_beyond_done = None
         self.market_value = 100
         return np.array(self.state)
@@ -105,10 +106,12 @@ class MABlockchainEnv(gym.Env):
         self.viewer = None
         self.state = None
 
-        self.market_value = 100
-        self.alpha = -0.1
-        self.ob = 1.1
-        self.os = 1.5
+        self.market_value = 1
+        self.alpha = -0.01
+        self.alphaX = -0.01
+        self.ob = 0.006
+        self.os = 0.005
+        self.list_v = []
 
         self.steps_beyond_done = None
 
@@ -120,37 +123,54 @@ class MABlockchainEnv(gym.Env):
         assert self.action_space.contains(listActions), "%r (%s) invalid"%(listActions, type(listActions))
         state = self.state
         listStates = list(state)
+        newListStates = list(state)
+        newListAction = [None] * len(self.state)
+        listRewards = [None] * len(self.state)
 
         for index in range(len(state)):
             action = min(listActions[index], listStates[index])
-            action = max(action, (listStates[index] - self.max_stake))
-            win_prob = state[index]*1.0/sum(state)
-            if(win_prob > np.random.rand(1)):
-                listStates[index] = listStates[index] - action + 1
+            action = max(action, (listStates[index] - self.max_stake + 1))
+            newListAction[index] = action
+
+        self.market_value = sum(newListAction) * self.alphaX + 1
+        # self.market_value = max(sum(listActions) * self.alpha + self.market_value, 50)
+        self.list_v.append(self.market_value)
+
+        for index in range(len(state)):
+            # # no mining
+            # newListStates[index] = listStates[index] - newListAction[index]
+
+            # mining progress
+            if(sum(state) != 0):
+                win_prob = state[index]*1.0/sum(state)
             else:
-                listStates[index] = listStates[index] - action
+                win_prob = 0
+            if(win_prob > np.random.rand(1)):
+                newListStates[index] = listStates[index] - newListAction[index] + 1
+            else:
+                newListStates[index] = listStates[index] - newListAction[index]
 
-        state = tuple(listStates)
-        self.state = state
-        self.market_value += sum(listActions) * self.alpha
-        listRewards = [None] * len(self.state)
-
-        for index in range(len(self.state)):
-            action = min(listActions[index], listStates[index])
-            action = max(action, (listStates[index] - self.max_stake))
-            if (listActions[index] > 0):  # selling
-                listRewards[index] = action * self.market_value - self.ob
-            elif (listActions[index] < 0):
-                listRewards[index] = action * self.market_value - self.os
+            # caculate the reward
+            if (newListAction[index] > 0):  # selling
+                listRewards[index] = newListAction[index] * self.market_value - self.ob
+            elif (newListAction[index] < 0):
+                listRewards[index] = newListAction[index] * self.market_value - self.os
             else:
                 listRewards[index] = 0
-        done = sum(state)==0
-        done = bool(done)
+
+        state = tuple(newListStates)
+        self.state = state
+
+        # done = sum(state)==0
+        # done = bool(done)
+        done = False
 
         return np.array(self.state), listRewards, done, {}
 
     def reset(self):
+        print(self.state)
         self.state = self.observation_space.sample()
+        print(self.state)
         self.steps_beyond_done = None
         self.market_value = 100
         return np.array(self.state)
--- blockchain/draw.py
+++ blockchain/draw.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 from xlrd import open_workbook
 
-book = open_workbook('result.xls')
-book2 = open_workbook('DDQN_result.xls')
-book3 = open_workbook('Dueling_DDQN_result.xls')
+book = open_workbook('../results/result.xls')
+book2 = open_workbook('../results/DDQN_result.xls')
+book3 = open_workbook('../results/Dueling_DDQN_result.xls')
 sheet = book.sheet_by_index(0)
 sheet2 = book2.sheet_by_index(0)
 sheet3 = book3.sheet_by_index(0)
